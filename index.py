384cd709f342cb7c28375133c83f13ab73fdd0de
Emmyxcel1960 <emmanuelowolabi1960@gmail.com>
2024-06-12 13:31:32 +0100

    created a python file that calculate grade

---
+++ index.py
@@ -0,0 +1,12 @@
+
+score=0
+if score > 75 and score <= 100:
+    print('A')
+elif score >=60 and score < 75:
+    print('b')
+elif score >= 50 and score < 60:
+    print('c')
+elif score >= 45 and score < 50:
+    print('D')
+else:
+    print('f')
